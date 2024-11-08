2d74acfb4a93a82bde069171cbd48b6e16ec225b
Amanda <nanda.hamandadesouza@gmail.com>
2024-11-08 11:01:08 -0300

    feat: adding endpoint for user

---
+++ src/Backend/WebApplication1/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Comunication.Request;
+using MyRecipeBook.Comunication.Response;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUsersJson request)
+        {
+            return Created();
+        }
+    }
+}
